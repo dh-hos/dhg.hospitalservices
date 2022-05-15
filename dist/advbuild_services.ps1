f94d1f58d87ea9d8c50e18ab1eca317a9454e32c
Ông Triệu Hậu <58720300+o-ngtrieuhau861gmailcom@users.noreply.github.com>
2022-05-15 17:16:07 +0700

    Update advbuild_services.ps1

--- dist/advbuild_services.ps1
+++ dist/advbuild_services.ps1
@@ -113,7 +113,7 @@ $arrayLines.Add('Rebuild')
 $arrayLines | Out-File -FilePath $Config.PathAdvancedInstallerCommandFile
 Write-Host $Config.PathAdvancedInstallerProjectFile
 Write-Host $Config.PathAdvancedInstallerCommandFile
-Start-Process -WindowStyle Hidden -Wait -FilePath "C:\adv\adv.exe" -ArgumentList ($Config.PathAdvancedInstallerProjectFile+' '+$Config.PathAdvancedInstallerCommandFile)
+Start-Process -WindowStyle Hidden -Wait -FilePath "C:\adv\adv.exe" -ArgumentList ('/execute "' + $Config.PathAdvancedInstallerProjectFile+'" "'+$Config.PathAdvancedInstallerCommandFile+'"')
 #"C:\adv\adv.exe" /execute $Config.PathAdvancedInstallerProjectFile $Config.PathAdvancedInstallerCommandFile
 Write-Host ('CompressZip {0}=>{1}' -f $Config.PathAdvancedInstallerOutputFile, $Config.PathAdvancedInstallerOutputFileZip)
 compress-archive -path $Config.PathAdvancedInstallerOutputFile -destinationpath ($Config.PathAdvancedInstallerOutputFileZip) -Force
